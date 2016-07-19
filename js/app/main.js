fd3579561029a9e1db49ccb304f7b4777080de7a
Greg Douglas <gwshark0@gmail.com>
2016-07-19 18:51:54 -0400

    it's 2016, time to start over

--- js/app/main.js
+++
@@ -1,7 +0,0 @@
-require(['app/koch'], function(koch) {
-	var degree = 4;
-	var segment_length = window.innerHeight / 2;
-	var line_width = 2;
-
-	koch.snowflake(degree, segment_length, line_width);
-});
